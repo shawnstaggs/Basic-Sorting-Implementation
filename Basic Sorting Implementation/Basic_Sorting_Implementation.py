adbebed5eade75a591fab0e9180e2814289ea0ec
Shawn Staggs <shawn.staggs@gmail.com>
2017-08-23 17:38:03 -0500

    initial setup of functions

--- Basic Sorting Implementation/Basic_Sorting_Implementation.py
+++ Basic Sorting Implementation/Basic_Sorting_Implementation.py
@@ -1,1 +1,22 @@
+# Set up the Bubble Sort algorythm here
+def bubble_sort():
+    pass
 
+# Set up the Merge Sort algorythm here
+def merge_sort():
+    pass
+
+# Set up the list gather, either user input or file input
+def list_input():
+    pass
+
+# Set up list output with request if the user wants a file output.
+def list_output():
+    pass
+
+# main calls input, bubble sort, merge sort. 
+# Then calls output
+def main():
+    list_input()
+    bubble_sort()
+    merge_sort()
