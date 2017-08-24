16fea078729e87402fa439b0801559f621b96134
Shawn Staggs <shawn.staggs@gmail.com>
2017-08-24 15:23:12 -0500

    Completed implementation of bubble and merge sorts

--- Basic Sorting Implementation/Basic_Sorting_Implementation.py
+++ Basic Sorting Implementation/Basic_Sorting_Implementation.py
@@ -1,22 +1,49 @@
-# Set up the Bubble Sort algorythm here
-def bubble_sort():
-    pass
+def bubble_sort(list_object):
+    working_list = list_object[:]
+    for pass_count in range(len(working_list)-1, 0, -1):
+        for step in range(pass_count):
+            if working_list[step] > working_list[step+1]:
+                working_list[step+1],working_list[step]=working_list[step],working_list[step+1]
+    return working_list
 
-# Set up the Merge Sort algorythm here
-def merge_sort():
-    pass
+def merge_sort(parent_list):
+    if len(parent_list) <= 1:
+        return parent_list
 
-# Set up the list gather, either user input or file input
-def list_input():
-    pass
+    left_half = []
+    right_half = []
 
-# Set up list output with request if the user wants a file output.
-def list_output():
-    pass
+    for each in range(len(parent_list)):
+        if each < len(parent_list) / 2:
+            left_half.append(parent_list[each])
+        else:
+            right_half.append(parent_list[each])
 
-# main calls input, bubble sort, merge sort. 
-# Then calls output
-def main():
-    list_input()
-    bubble_sort()
-    merge_sort()
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] <= right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+
+    while len(left) > 0:
+        result.append(left.pop(0))
+    while len(right) > 0:
+        result.append(right.pop(0))
+
+    return result
+
+lst = raw_input("Go ahead and enter your list: ").split(',')
+
+bubble_sorted = bubble_sort(lst)
+merge_sorted = merge_sort(lst)
+
+print lst
+print bubble_sorted
+print merge_sorted
